e7b17c4861b0fda53d893da95a2ebfcaadb973b8
Daxin319 <lylehigg@gmail.com>
2024-10-03 22:33:13 -0600

    added char counter test 1

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def main():
     file_path = "books/frankenstein.txt"
     text = get_book_text(file_path)
     num_words = word_count(file_path)
-    print(f"{num_words} words found in the document")
+    num_chars = char_count(text)
+    print(num_chars)
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -14,5 +15,14 @@ def word_count(file_path):
     words = get_book_text(file_path).split()
     return len(words)
 
+def char_count(text):
+    lowercase = text.lower()
+    chars = {}
+    for char in text:
+        if char not in chars:
+            chars[f."{char}"] = 1
+        else:
+            chars[f."{char}"] += 1
+    return chars
 
 main()
