cef3e6c7e990a1cecc62cbda64727def0c922632
Daxin319 <lylehigg@gmail.com>
2024-10-03 21:56:37 -0600

    added word counter and cleaned up code

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # Main program file for BookBot
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-    return
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)
+    num_words = word_count(file_path)
+    print(f"{num_words} words found in the document")
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
+
+def word_count(file_path):
+    words = get_book_text(file_path).split()
+    return len(words)
+
 
 main()
